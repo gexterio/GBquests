83089aaf1930c0a0e881cfd64bb7378b3b26403e
gexterio <gexterio@gmail.com>
2022-04-24 15:01:45 +0400

    Tic_Tak_Toe v0.2 (not ready for use)

--- src/main/java/ru/gb/gexterio/Main.java
+++ src/main/java/ru/gb/gexterio/Main.java
@@ -5,17 +5,20 @@ import java.util.Scanner;
 public class Main {
 
     private static boolean GAME_OVER = false;
-    private static final int FIELD_SIZE = 5;
+    private static  int CELL_TO_WIN = 3;
+    private static  int FIELD_SIZE = 3;  //размер поля
     private static final int EMPTY_CELL = 42; // *
-    private static final int X = 88;  //X
-    private static final int O = 79;  //O
+    private static final int X = 88;  //X (крестик)
+    private static final int O = 79;  //O (нолик)
     private static final String HUMAN_TURN_MESSAGE = "Сделай свой ход, храбрец в формате: столбец, строка";
     private static final String AI_TURN_MESSAGE = "Свой ход делает Искусственный интеллект";
     private static final String GAME_OVER_MESSAGE = "GAME_OVER, Сэр";
     private static final String WIN_MESSAGE = "Ты победил, круто!";
-    private static char[][] actualField;
+    private static final String LOSE_MESSAGE = "Ты проиграл, рандом оказался сильнее!";
+    private static final String NOBODY_MESSAGE = "Ничья! Люди и Компьютер равны";
+    private static char[][] actualField; //актуальное состояние игрового поля
 
-    private static int turnCount = 0;
+    private static int turnCount = 0; //счетчик ходов
 
 
     public static void main(String[] args) {
@@ -24,89 +27,69 @@ public class Main {
         while (!GAME_OVER) {
             humanTurn();
             gameOverChecker();
-            //   AITurn();
-            //   gameOverChecker();
+            AITurn();
+            gameOverChecker();
         }
     }
 
     private static void gameOverChecker() {
-        if (turnCount == FIELD_SIZE * FIELD_SIZE + 1) {
-            System.out.println(GAME_OVER_MESSAGE);
-            GAME_OVER = true;
-            System.exit(0);
-        } else if (isWin()) {
-            GAME_OVER = true;
-            System.out.println(WIN_MESSAGE);
+        if (turnCount == FIELD_SIZE * FIELD_SIZE) { //проверка на заполнение поля
+            doNobody();
+
         }
+
     }
-
-    private static boolean isWin() {
-        int XCellCountRow = 0;
-        int XCellCountColum = 0;
-        int XCellCountMajorDiagonal = 0;
-        int XCellCountMinorDiagonal = 0;
-        for (int i = 0; i < FIELD_SIZE; i++) {
-            for (int j = 0; j < FIELD_SIZE; j++) {
-                if (actualField[i][j] == X) {  // проверка на 3 в ряд
-                    XCellCountRow++;
-                }
-            }
-        }
-        for (int i = 0; i < FIELD_SIZE; i++) {
-            for (int j = 0; j < FIELD_SIZE; j++) {
-                if (actualField[i][i] == X) { // проверка на 3 в столбец
-                    XCellCountColum++;
-                }
-            }
-        }
-        for (int i = 0; i < FIELD_SIZE; i++) {
-            for (int j = 0; j < FIELD_SIZE; j++) {
-                if (i == j && actualField[i][j] == X) { //проверка на 3 по главной диагонали
-                    XCellCountMajorDiagonal++;
-                }
-            }
-        }
-        for (int i = 0; i < FIELD_SIZE; i++) {
-            for (int j = 0; j < FIELD_SIZE; j++) {
-                if (j == FIELD_SIZE - 1 - i && actualField[i][j] == X) {  //проверка на 3 по побочной диагонали
-                    XCellCountMinorDiagonal++;
-                }
-            }
-        }
-
-        if (XCellCountRow ==1 || XCellCountColum ==1 || XCellCountMajorDiagonal ==1 || XCellCountMinorDiagonal ==1) {
-            return true;
-        } else {
-            return false;
+    private static void direction (int row, int column, int dir) {
+        if () {
         }
     }
 
 
-    private static void humanTurn() {
-        System.out.println(HUMAN_TURN_MESSAGE);
+    private static void doWin() {
+        GAME_OVER = true;
+        System.out.println(GAME_OVER_MESSAGE + ". " + WIN_MESSAGE);
+        System.exit(0);
+    }
+
+    private static void doLose() {
+        GAME_OVER = true;
+        System.out.println(GAME_OVER_MESSAGE + ". " + LOSE_MESSAGE);
+        System.exit(0);
+    }
+    private static void doNobody () {
+        GAME_OVER = true;
+        System.out.println(GAME_OVER_MESSAGE + "." + NOBODY_MESSAGE);
+        System.exit(0);
+    }
+
+
+    private static void humanTurn() {  //Ход игрока
         Scanner scanner = new Scanner(System.in);
         boolean isEmpty = true;
+        System.out.println(HUMAN_TURN_MESSAGE);
         while (isEmpty) {
-            int colum = scanner.next().toUpperCase().charAt(0) - 'A';
+            int column = scanner.next().toUpperCase().charAt(0) - 'A';
             int row = scanner.nextInt();
-            if (actualField[row][colum] == EMPTY_CELL) {
-                actualField[row][colum] = X;
-                isEmpty = false;
+            if (actualField[row][column] == EMPTY_CELL) {
+                actualField[row][column] = X;
                 printField();
+                turnCount++;
+                isEmpty = false;
             }
         }
     }
 
-    private static void AITurn() {
+    private static void AITurn() {  //Ход искусственного интеллекта
         boolean isEmpty = true;
         System.out.println(AI_TURN_MESSAGE);
         while (isEmpty) {
-            int colum = (int) (Math.random() * FIELD_SIZE);
+            int column = (int) (Math.random() * FIELD_SIZE);
             int row = (int) (Math.random() * FIELD_SIZE);
-            if (actualField[row][colum] == EMPTY_CELL) {
-                actualField[row][colum] = O;
-                isEmpty = false;
+            if (actualField[row][column] == EMPTY_CELL) {
+                actualField[row][column] = O;
                 printField();
+                turnCount++;
+                isEmpty = false;
             }
         }
     }
@@ -126,10 +109,17 @@ public class Main {
             System.out.println();
         }
         System.out.println();
-        turnCount++;
     }
 
     public static void initialField() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Укажи размер поля");
+        FIELD_SIZE = scanner.nextInt();
+        if (FIELD_SIZE>3) {
+            CELL_TO_WIN = 4;
+        } else {
+            CELL_TO_WIN = FIELD_SIZE;
+        }
         actualField = new char[FIELD_SIZE][FIELD_SIZE]; //создание пустого поля
         for (int i = 0; i < FIELD_SIZE; i++) {
             for (int j = 0; j < FIELD_SIZE; j++) {
